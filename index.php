9ddc6040a8386f294ffa41734b93c823195a893b
Teja Simha Reddy <88872550+TejaSimhaReddy3@users.noreply.github.com>
2021-09-21 15:06:12 +0530

    Main Page

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<title>home page</title>
+	<style>
+	.hr{
+		color:white;
+		border-bottom: 3px solid rgb(245, 243, 243);
+	}
+	</style>
+<head>
+<meta charset="UTF-8">
+<link href="1.css" rel="stylesheet" type="text/css"  media="all" />
+<body>
+
+		<img class='bg' src='bg.jpg' alt='background'>
+		<h1 align='center'>INFIRMARY SUPERINTENDENCE</h1>
+		<hr class="hr">
+
+	
+				<a href="adminlog.php"><img class="img1" src="admin2.png"></a>
+				<a href="doctorlog.php"><img class="img2" src="doctor.png"></a></br>
+				<a href="plog.php"><img class="img3" src="patient.png"></a>
+</body>
+</html>
+
